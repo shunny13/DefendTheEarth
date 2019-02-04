d476d3c764facf003f12afe5ac72b54b4f990917
nutu <nutu.campan@hotmail.com>
2019-02-04 01:22:20 +0100

    ennemy easier with color incorporated

--- js/ennemy.js
+++ js/ennemy.js
@@ -1,13 +1,13 @@
-function ennemy(difficulty){
+function ennemy(difficulty,color){
 	this.x = random(smallX+20,smallX+smallW-20);
 	this.y = random(-50,-20);
-	this.spd = random(1*difficulty,3*difficulty);
+	this.spd = random(1*difficulty,2*difficulty);
 	this.hp = random(1,5);
 	this.r = random(10,20);
 	this.toDelete = false;
 
 	this.show= function(){
-		fill('red');
+		fill(color);
 		circle(this.x,this.y,this.r);
 	}
 	this.move = function(){
